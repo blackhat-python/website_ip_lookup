66edcc06badb266e750ab94b461131c0b5ba42be
Godpython <95225963+godpython1@users.noreply.github.com>
2024-05-07 20:11:04 +0530

    Create findip.py

---
+++ findip.py
@@ -0,0 +1,14 @@
+GNU nano 8.0                         main.py
+import socket
+
+def get_ip_address(url):
+    try:
+        ip_address = socket.gethostbyname(url)
+        return ip_address
+    except socket.gaierror:
+        return "Unable to resolve hostname"
+
+# Example usage
+website_url = input("Enter Website URL : ")
+ip_address = get_ip_address(website_url)
+print("IP Address of ", website_url, ":", ip_address)
